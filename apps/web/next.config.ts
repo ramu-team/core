e6cd0a491e0126b92e3e2a782dce6096ac507d84
rifkibayuariy <email.rifkibayu@gmail.com>
2026-08-04 21:56:46 +0700

    feat(web): add web application for consumer facing platform

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["@workspace/ui"],
+  transpilePackages: ["@ramu/ui", "@ramu/db"],
 }
 
 export default nextConfig
+
--- apps/web/postcss.config.mjs
+++ apps/web/postcss.config.mjs
@@ -1,1 +1,1 @@
-export { default } from "@workspace/ui/postcss.config";
+export { default } from "@ramu/ui/postcss.config"
